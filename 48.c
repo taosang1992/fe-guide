672e3d9a60d9116bb984e2c676ada6bc2c0871d6
taosang1992 <taosang1992@gmail.com>
2019-07-08 08:06:46 +0800

    添加阶乘代码

--- 48.c
+++ 48.c
@@ -8,3 +8,15 @@ int main() {
 	printf("%d!=%d\n",n,production(n));
 	return 0;
 }
+
+int production(int n){
+	int result;
+	if(n<0){
+		printf("n<0,数字输入得不对！");
+	}else if(n==0 || n==1){
+		result = 1;
+	}else{
+		result = production(n-1)*n;
+	}
+	return result;
+}
